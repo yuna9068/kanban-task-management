35b0fc622e44a638545f63e73d07d5c73974c082
Yuna <yuna9068@gmail.com>
2023-11-22 15:56:35 +0800

    feat: 新增 reset css 和基礎樣式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: [
+    '~/assets/style/reset.css',
+    '~/assets/style/main.scss',
+  ],
   modules: [
     '@vueuse/nuxt',
     'nuxt-svgo',
     '@pinia/nuxt',
     '@nuxtjs/google-fonts',
   ],
+  googleFonts: {
+    families: {
+      'Plus Jakarta Sans': {
+        wght: [500, 700],
+      },
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "~/assets/style/config/index.scss";',
+        },
+      },
+    },
+  },
 })
