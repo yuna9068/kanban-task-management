3ccbcbcc6e5d8f9901d652214a525033dc31539f
Yuna <yuna9068@gmail.com>
2023-12-14 18:12:35 +0800

    feat: 換頁前若正在編輯看板或任務則顯示警示訊息，若無則關閉所有 Modal 後進行換頁

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,98 @@
 import { useLocalStorage } from '@vueuse/core'
 import initialData from '@/assets/data/boards.json'
 
+const router = useRouter()
+
 const boardStore = useBoardStore()
-const { setBoardData } = boardStore
-const { getIsLoading } = storeToRefs(boardStore)
+const sidebarStore = useSidebarStore()
+const taskStore = useTaskStore()
+const alertStore = useAlertStore()
+const { setBoardData, closeModalBoard } = boardStore
+const { closeSidebar } = sidebarStore
+const { closeModalTaskDetail, closeModalTaskEdit } = taskStore
+const { openModalAlert } = alertStore
+const { getIsLoading, getModalBoardInfo } = storeToRefs(boardStore)
+const { getModalTaskDetail, getModalTaskEdit } = storeToRefs(taskStore)
 
 const storage = useLocalStorage('board', [...initialData.boards])
+const { isMobile } = useDevice()
+
+const toPagePath = ref('')
+
+const isEditing = computed(() => (
+  getModalBoardInfo.value.display
+  || getModalTaskDetail.value.display
+  || getModalTaskEdit.value.display
+))
 
 boardStore.$subscribe((mutate, state) => {
   storage.value = state.boardList
 })
 
+/**
+ * 前往頁面
+ */
+function goToPage() {
+  router.push(toPagePath.value)
+}
+
+/**
+ * 關閉所有 Modal，手機版要關 sidebar
+ */
+function closeAllModal() {
+  closeModalBoard()
+  closeModalTaskDetail()
+  closeModalTaskEdit()
+
+  if (isMobile.value)
+    closeSidebar()
+}
+
+/**
+ * 顯示警示訊息：用於正在編輯看板或任務時跳轉頁面
+ */
+function openAlert() {
+  openModalAlert({
+    title: 'Leave this page',
+    content: 'Are you sure you want to leave? Changes you made may not be saved.',
+    destructiveText: 'Leave',
+    onDestructive: () => {
+      closeAllModal()
+      goToPage()
+    },
+  })
+}
+
+/**
+ * 關閉網頁、上一頁、下一頁、重整頁面時確認是否正在編輯看板或任務
+ * @param e BeforeUnloadEvent
+ */
+function beforeunload(e: BeforeUnloadEvent) {
+  if (isEditing.value) {
+    e.preventDefault()
+    e.returnValue = ''
+  }
+}
+
 onMounted(() => {
   setBoardData(storage.value)
+  window.addEventListener('beforeunload', beforeunload)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('beforeunload', beforeunload)
+})
+
+onBeforeRouteLeave((to) => {
+  toPagePath.value = to.path
+
+  if (isEditing.value) {
+    openAlert()
+    return false
+  }
+  else {
+    closeAllModal()
+  }
 })
 </script>
 
